6fdf60bbbc7c1def7f0ec88f625af9daa0992d26
Vitor Thomaz <vthomaz1996@gmail.com>
2021-01-31 22:15:20 -0300

    feature: Added delete and edit icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as AddCircle } from './addCircle';
 export { default as Close } from './close';
+export { default as Pencil } from './pencil';
+export { default as Trash } from './trash';
